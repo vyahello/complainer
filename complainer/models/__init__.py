3b4b71f48712d05a0dc459b59b07356fe71a295b
Volodymyr <vyahello@gmail.com>
2022-01-21 22:10:38 +0200

    Add transaction table

--- complainer/models/__init__.py
+++ complainer/models/__init__.py
@@ -2,3 +2,4 @@
 
 from complainer.models.complaint import *  # noqa: F401, F403
 from complainer.models.user import *  # noqa: F401, F403
+from complainer.models.transaction import *  # noqa: F401, F403
---
+++ complainer/models/transaction.py
@@ -0,0 +1,19 @@
+"""Provide API for transaction model scheme."""
+import sqlalchemy
+
+from complainer.db import metadata
+
+transaction = sqlalchemy.Table(
+    'transactions',
+    metadata,
+    sqlalchemy.Column('id', sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column('quote_id', sqlalchemy.String(120), nullable=False),
+    sqlalchemy.Column('transfer_id', sqlalchemy.Integer, nullable=False),
+    sqlalchemy.Column(
+        'target_account_id', sqlalchemy.String(120), nullable=False
+    ),
+    sqlalchemy.Column('amount', sqlalchemy.Float, nullable=False),
+    sqlalchemy.Column(
+        'complaint_id', sqlalchemy.ForeignKey('complaints.id'), nullable=False
+    ),
+)
