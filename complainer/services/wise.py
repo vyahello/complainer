65e19f5e38d9b518e9361538d76c53296cce494b
Volodymyr <vyahello@gmail.com>
2022-01-21 21:06:10 +0200

    Add "create quote" wise method

--- complainer/services/wise.py
+++ complainer/services/wise.py
@@ -1,6 +1,7 @@
 """Wise payment service API."""
+import json
 from http.client import HTTPException
-from typing import List
+from typing import Any, Dict, List
 
 import requests
 from decouple import config
@@ -10,7 +11,7 @@ class WiseService:  # pylint: disable=too-few-public-methods
     """Represents wise payment service."""
 
     def __init__(self) -> None:
-        self.main_url = '...'
+        self.main_url = config('WISE_URL')
         self.headers = {
             'ContentType': 'application/json',
             'Authorization': f'Bearer {config("WISE_TOKEN")}',
@@ -18,8 +19,8 @@ class WiseService:  # pylint: disable=too-few-public-methods
         self.profile_id = self._get_profile_id()
 
     def _get_profile_id(self) -> List[str]:
-        """Return wise user profile id"""
-        url = 'https://api.sandbox.transferwise.tech/v1/profiles'
+        """Return wise user profile id."""
+        url = f'{self.main_url}/v1/profiles'
         resp = requests.get(url, headers=self.headers)
         if resp.status_code == 200:
             return [
@@ -27,6 +28,22 @@ class WiseService:  # pylint: disable=too-few-public-methods
                 for element in resp.json()
                 if element['type'] == 'personal'
             ][0]
+        raise HTTPException(
+            500, 'Payment provider is not available at the moment'
+        )
+
+    def create_quote(self, amount: int) -> Dict[str, Any]:
+        """Create quote in wise payment service."""
+        url = f'{self.main_url}/v2/quotes'
+        data = {
+            'sourceCurrency': 'EUR',
+            'targetCurrency': 'EUR',
+            'sourceAmount': amount,
+            'profile': self.profile_id,
+        }
+        resp = requests.post(url, headers=self.headers, data=json.dumps(data))
+        if resp.status_code == 200:
+            return resp.json()
         raise HTTPException(
             500, 'Payment provider is not available at the moment'
         )
